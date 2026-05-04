d0950cf2769e32167d7865557f97e16d3debd553
James <joshuajegedep@gmail.com>
2026-05-04 17:39:13 -0500

    Drop dead bucket_key_enabled, wire build output to apply

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -35,7 +35,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "photos" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-    bucket_key_enabled = true
+    # bucket_key_enabled is only meaningful with sse_algorithm = "aws:kms".
+    # Will be re-enabled together with the KMS CMK follow-up.
   }
 }
 
